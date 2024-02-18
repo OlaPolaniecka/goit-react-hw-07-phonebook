a8bf2fd02c06e95bf934b8cbbed748e66a3bd7e7
Ola Połaniecka <97olka@gmail.com>
2024-02-18 16:16:04 +0100

    changes

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import ContactForm from 'components/ContactForm/ContactForm';
+import ContactList from 'components/ContactList/ContactList';
+import Filter from 'components/Filter/Filter';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  addContact,
+  deleteContact,
+  selectVisibleContacts,
+} from './redux/contactsSlice';
+
+const App = () => {
+  const dispatch = useDispatch();
+  const visibleContacts = useSelector(selectVisibleContacts);
+
+  const handleAddContact = ({ id, name, number }) => {
+    dispatch(addContact({ id, name, number }));
+  };
+
+  const handleRemoveContact = contact => {
+    dispatch(deleteContact(contact.id));
+  };
+  return (
+    <div>
+      <h1 className="header">Phonebook</h1>
+      <ContactForm onClick={handleAddContact} />
+      <h1 className="header">Contacts</h1>
+      <Filter />
+      <ContactList contacts={visibleContacts} onClick={handleRemoveContact} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,25 @@
+import { useDispatch } from 'react-redux';
+import { deleteContact } from '../../redux/contactsSlice';
+
+const ContactList = ({ contacts }) => {
+  const dispatch = useDispatch();
+
+  const handleRemoveContact = contact => {
+    dispatch(deleteContact(contact.id));
+  };
+
+  return (
+    <ul>
+      {contacts.map(contact => (
+        <li key={contact.id}>
+          {contact.name} : {contact.number}
+          <button className="btn" onClick={() => handleRemoveContact(contact)}>
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
